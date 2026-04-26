4874243ea755dcf5503db39c859d994ff0f8af36
TubagusAulia <tebe.aulia@gmail.com>
2026-04-26 18:12:36 +0700

    adjusting the watchlist page

--- src/app/(features)/watchlist/page.tsx
+++ src/app/(features)/watchlist/page.tsx
@@ -58,7 +58,7 @@ export default function WatchlistPage() {
   }
 
   return (
-    <div className="min-h-screen bg-slate-950 selection:bg-amber-500/30 selection:text-amber-200">
+    <div className="min-h-screen bg-black selection:bg-amber-500/30 selection:text-amber-200">
       <Navbar />
       <div className="max-w-7xl mx-auto py-12 px-4 pt-24 relative">
         <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
